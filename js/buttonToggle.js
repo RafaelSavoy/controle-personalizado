7d17cfe530146694dceb0c74c512fa3d128402ba
Rafael Savoy <rafaelsavoy1@outlook.com>
2022-01-24 00:00:16 -0300

    Consertando o bug de carregamento

--- js/buttonToggle.js
+++ js/buttonToggle.js
@@ -3,9 +3,3 @@ const body = document.getElementsByTagName('body')[0]
 function hamburguerToggle(){
 	body.classList.toggle('active')
 }
-
-function resizeBtn(){
-	if(window.outerWidth > 566){
-		body.classList.remove('active')
-	}
-}
